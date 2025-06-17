82738da5fa028e504dacf2b948cd696607e87a46
rafacostadev <rafaelcostamantis@gmail.com>
2025-06-16 23:10:17 -0300

    inicial

---
+++ src/test/java/com/modelo/dominio/orm/desafio/modelo/dominio/orm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelo.dominio.orm.desafio.modelo.dominio.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
